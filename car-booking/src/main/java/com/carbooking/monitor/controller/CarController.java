1bcc22b7292be07abb5379eb75c4a1fb11304bb8
John Kevin <johnkevin.zurita@gmail.com>
2025-11-15 17:37:37 +0000

    carController.java changed Long to String

--- car-booking/src/main/java/com/carbooking/monitor/controller/CarController.java
+++ car-booking/src/main/java/com/carbooking/monitor/controller/CarController.java
@@ -26,7 +26,7 @@ public class CarController {
     }
 
     @GetMapping("/{id}")
-    public Car getCarById(@PathVariable Long id) {
+    public Car getCarById(@PathVariable String id) {
         return carRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Car not found with ID: " + id));
     }
@@ -43,7 +43,7 @@ public class CarController {
     // }
 
     @DeleteMapping("/{id}")
-    public void deleteCar(@PathVariable Long id) {
+    public void deleteCar(@PathVariable String id) {
         carRepository.deleteById(id);
     }
 
